e481911d3f06c8f97ee5e727d73bd41b6f4b8851
yutytuty <49990984+yutytuty@users.noreply.github.com>
2024-01-03 23:29:12 +0200

    Clean up ram and rom

--- src/mem/ram.vhd
+++ src/mem/ram.vhd
@@ -5,39 +5,36 @@ library ieee;
 
 entity ram is
   port (
-    clk      : in  std_logic;
-    addr_bus : in  std_logic_vector(15 downto 0);
-    load     : in  std_logic;
-    input    : in  std_logic_vector(7 downto 0);
-    o        : out std_logic_vector(7 downto 0));
+    clk   : in  std_logic;
+    addr  : in  std_logic_vector(15 downto 0);
+    we    : in  std_logic;
+    input : in  std_logic_vector(7 downto 0);
+    o     : out std_logic_vector(7 downto 0));
 end entity;
 
 architecture ram_arch of ram is
-  type ram_array is array (0 to 32767) of std_logic_vector(7 downto 0);
+  constant RAM_START  : natural := 32768; -- mapped location of ram
+  constant RAM_HEIGHT : natural := 32768;
+  constant RAM_WIDTH  : natural := 8;
+  subtype word_t is std_logic_vector((RAM_WIDTH - 1) downto 0);
+  type memory_t is array (0 to RAM_HEIGHT - 1) of word_t;
 
-  signal en  : std_logic := '0';
-  signal ram : ram_array := (others => x"00");
-  -- signal addr_reg : std_logic_vector(15 downto 0) := x"0000";
+  signal ram      : memory_t := (others => x"00");
+  signal addr_reg : natural range 0 to 2 ** RAM_WIDTH - 1;
 begin
 
-  p_enable: process (addr_bus)
+  process (clk)
   begin
-    if addr_bus >= 32768 and addr_bus < 65536 then
-      en <= '1';
-    else
-      en <= '0';
+    if rising_edge(clk) then
+      if we = '1' then
+        ram(to_integer(unsigned(addr))) <= input;
+      end if;
+
+      if to_integer(unsigned(addr)) >= RAM_START and to_integer(unsigned(addr)) < RAM_START + RAM_HEIGHT then
+        addr_reg <= to_integer(unsigned(addr)) - RAM_START;
+      end if;
     end if;
   end process;
 
-  p_read_write: process (clk, load)
-  begin
-    if rising_edge(clk) then
-      if en = '1' then
-        if load = '1' then
-          ram(to_integer(unsigned(addr_bus)) - 32768) <= input;
-        end if;
-      end if;
-      o <= ram(to_integer(unsigned(addr_bus)) - 32768);
-    end if;
-  end process;
+  o <= ram(addr_reg);
 end architecture;
--- src/mem/rom.vhd
+++ src/mem/rom.vhd
@@ -4,40 +4,39 @@ library ieee;
   use ieee.numeric_std.all;
 
 entity rom is
+
   port (
-    clk      : in  std_logic;
-    addr_bus : in  std_logic_vector(15 downto 0);
-    o        : out std_logic_vector(7 downto 0));
+    clk  : in  std_logic;
+    addr : in  std_logic_vector(15 downto 0);
+    o    : out std_logic_vector(7 downto 0));
 end entity;
 
 architecture rom_arch of rom is
-  type rom_array is array (0 to 32767) of std_logic_vector(7 downto 0);
+  constant ROM_START  : natural := 0; -- mapped location of rom
+  constant ROM_HEIGHT : natural := 32768;
+  constant ROM_WIDTH  : natural := 8;
+  subtype word_t is std_logic_vector((ROM_WIDTH - 1) downto 0);
+  type memory_t is array (0 to ROM_HEIGHT - 1) of word_t;
 
-  signal en : std_logic := '0';
-
-  constant rom : rom_array := (
-    0 => x"01",
-    1 => x"02",
-    2 => x"03",
-    others => (Others => '0')
+  constant rom : memory_t := (
+    -- some example values
+    0      => x"01",
+    1      => x"A0",
+    2      => x"55",
+    3      => x"AA",
+    others => x"00"
   );
+  signal addr_reg : natural range 0 to 2 ** ROM_WIDTH - 1;
 begin
-
-  p_enable: process (addr_bus)
-  begin
-    if addr_bus < 32768 and addr_bus >= 0 then
-      en <= '1';
-    else
-      en <= '0';
-    end if;
-  end process;
-
-  p_read: process (clk)
+  process (clk)
   begin
     if rising_edge(clk) then
-      if en = '1' then
-        o <= rom(to_integer(unsigned(addr_bus)));
+
+      if to_integer(unsigned(addr)) >= ROM_START and to_integer(unsigned(addr)) < ROM_START + ROM_HEIGHT then
+        addr_reg <= to_integer(unsigned(addr)) - ROM_START;
       end if;
     end if;
   end process;
+
+  o <= rom(addr_reg);
 end architecture;
