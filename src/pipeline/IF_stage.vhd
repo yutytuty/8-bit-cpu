27218746a95cac872a97969d47a1dbaf78ca40a2
yutytuty <49990984+yutytuty@users.noreply.github.com>
2024-01-06 17:40:59 +0200

    Add support for 24bit instructions

--- src/pipeline/IF_stage.vhd
+++ src/pipeline/IF_stage.vhd
@@ -4,32 +4,41 @@ library ieee;
 
 entity IF_stage is
   port (
-    clk : in  std_logic;
-    rst : in  std_logic;
-    o   : out std_logic_vector(15 downto 0));
+    clk                 : in  std_logic;
+    rst                 : in  std_logic;
+    -- increment pc by 1.5 instead of 1. Set if previous was I-type instruction (by ID stage).
+    previous_was_i_type : in  std_logic;
+    inst                : out std_logic_vector(15 downto 0);
+    extra_8             : out std_logic_vector(7 downto 0));
 end entity;
 
 architecture IF_stage_arch of IF_Stage is
   component pcu is
     port (
-      clk : in  std_logic;
-      rst : in  std_logic;
-      o   : out std_logic_vector(15 downto 0));
+      clk          : in  std_logic;
+      rst          : in  std_logic;
+      inc_1andhalf : in  std_logic;
+      o            : out std_logic_vector(15 downto 0);
+      extra_8      : out std_logic_vector(7 downto 0));
   end component;
 
-  signal IR : std_logic_vector(15 downto 0);
+  signal IR     : std_logic_vector(15 downto 0);
+  signal next_8 : std_logic_vector(7 downto 0);
 begin
   c_PCU: pcu
     port map (
-      clk => clk,
-      rst => rst,
-      o   => IR
+      clk          => clk,
+      rst          => rst,
+      inc_1andhalf => previous_was_i_type,
+      o            => IR,
+      extra_8      => next_8
     );
 
   process (clk)
   begin
     if rising_edge(clk) then
-      o <= IR;
+      inst <= IR;
+      extra_8 <= next_8;
     end if;
   end process;
 end architecture;
